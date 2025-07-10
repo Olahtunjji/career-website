06f01ddb0b2360ca55f639289895d850ad9a3b89
OGUNTONADE OLUWASENI <40181515+Olahtunjji@users.noreply.github.com>
2025-07-10 16:40:30 +0000

    career website stuct

--- app.py
+++ app.py
@@ -1,11 +1,40 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
+PROJECTS = [
+  {
+    'id': 1,
+    'title': 'Data Analyst',
+    'description': 'Analyze data and make decisions',
+   },
+  {
+    'id': 2,
+    'title': 'Data Clerk',
+    'description': 'Enter data into a database',
+   },
+{
+  'id': 3,
+  'title': 'Data Scientist',
+  'description': 'create models and make predictions',
+  },
+{
+  'id': 4,
+  'title': 'Data Rect',
+  'description': 'rectify data and make it clean',
+ }
+]
+
 @app.route('/')
-def hello_world():
-  return render_template('home.html')
+def home():
+  return render_template('home.html',
+                        projects=PROJECTS,
+                        company_name='Replit')
+@app.route("/api/projects")
+def list_projects():
+  return jsonify(PROJECTS)
+
 
 if __name__ == '__main__':
-  app.run( host='0.0.0.0', debug=True, )
+  app.run(host='0.0.0.0', debug=True,)
   
